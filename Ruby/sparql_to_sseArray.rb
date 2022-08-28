214b679190203fbdb335d783018424231ae95b06
ichichi-3180 <watanabe.soichiro.qa@alumni.tsukuba.ac.jp>
2022-08-28 22:39:27 +0900

    <update> comment_out

--- Ruby/sparql_to_sseArray.rb
+++ Ruby/sparql_to_sseArray.rb
@@ -1,6 +1,6 @@
 #読み込むファイルのパスを指定
 # file_path = "../SPARQL50本ノック/SPARQL_013.sparql"
-input_file_path = "../SPARQL_query_sample/ndl_subquery_out.sparql"
+input_file_path = "../SPARQL_query_sample/ndl_subquery.sparql"
 # input_file_path = "../SPARQL50本ノック/SPARQL_034.sparql"
 
 input_file = File.open(input_file_path, "r")
@@ -13,8 +13,11 @@ require 'sparql'
 query = SPARQL::Grammar::Parser.new(query) #SPARQLライブラリのオブジェクトして扱う
 p query.class
 query = query.parse() #SPARQLクエリを解析
-p query.class
+# p query.class.ancestors
+# p query.class.method(:to_sxp_bin).owner
+# p query.class.method(:to_sxp_bin).source_location
 query_sseArray = query.to_sxp_bin #解析したクエリをSPARQLクエリをsse形式のarrayに変換
+
 p query_sseArray.class
 pp query_sseArray
 #書き出すファイルの指定
