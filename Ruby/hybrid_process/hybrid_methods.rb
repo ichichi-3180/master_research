09440bf165995510103c9cd07ff7e6d185d5bcea
ichichi-3180 <watanabe.soichiro.qa@alumni.tsukuba.ac.jp>
2022-08-29 17:22:41 +0900

    method #subquery_hybrid  done

--- Ruby/hybrid_process/hybrid_methods.rb
+++ Ruby/hybrid_process/hybrid_methods.rb
@@ -47,27 +47,27 @@ end
 
 
 
-def get_out_sseArray(a, project_count)
-    array = []
-    while a.length != 0 do
-        e = a.shift
-        if e.class == Symbol then
-            if e == :project then
-                project_count += 1
-            end
-            if project_count != 2 then
-                array.push(e)
-            else
-                break
-            end          
-        elsif e.class == Array then
-            array.push(get_out_sseArray(e, project_count))
-        else
-            array.push(e)
-        end
-    end
-    return array
-end
+# def get_out_sseArray(a, project_count)
+#     array = []
+#     while a.length != 0 do
+#         e = a.shift
+#         if e.class == Symbol then
+#             if e == :project then
+#                 project_count += 1
+#             end
+#             if project_count != 2 then
+#                 array.push(e)
+#             else
+#                 break
+#             end          
+#         elsif e.class == Array then
+#             array.push(get_out_sseArray(e, project_count))
+#         else
+#             array.push(e)
+#         end
+#     end
+#     return array
+# end
 
 # prefix句のsseArrayを取得するメソッド
 def get_prefix_sseArray(a)
@@ -86,39 +86,64 @@ def get_prefix_sseArray(a)
     end
 end
 
-# def get_out_sseArray(a, project_count)
-#     operator = ""
-#     return_array = []
-#     a.length.times do |i|
-#         if i == 0 && a[i].class == Symbol then
-#             if a[i] == :project then
-#                 project_count += 1
-#             end
-#             if project_count == 2 then
-#                 break
-#             else
-#                 return_array.push(a[i])
-#             end
-#         elsif a[i].class == Array then
-#             p a[i]
-#             add_flag = get_out_sseArray(a[i], project_count)
-#             if add_flag then
-#                 return_array.push(add_flag)
-#             else
-#                 return_array.push([])
-#             end
-#         else
-#             return_array.push(a[i])
-#         end
-#     end
-#     p project_count
-#     p return_array
-#     if project_count == 2 then
-#         return false
-#     else
-#         return return_array
-#     end
-# end
+def get_out_sseArray(a, project_count)
+    operator = ""
+    return_array = []
+    a.length.times do |i|
+        if i == 0 && a[i].class == Symbol then
+            if a[i] == :project then
+                project_count += 1
+            end
+            if project_count == 2 then
+                break
+            else
+                return_array.push(a[i])
+            end
+        elsif a[i].class == Array then
+            # p a[i]
+            add_flag = get_out_sseArray(a[i], project_count)
+            if add_flag then
+                return_array.push(add_flag)
+            else
+                return_array.push([])
+            end
+        else
+            return_array.push(a[i])
+        end
+    end
+    # p project_count
+    # p return_array
+    if project_count == 2 then
+        return false
+    else
+        return return_array
+    end
+end
+
+#base_sseArray内のoperatorに対して、次の引数に指定したadd_sseArrayを追加する(operator内の配列の要素は2つまでなので、末尾の要素を削除する)
+def add_operator_next_array(base_sseArray, operator, add_sseArray)
+    if base_sseArray.length == 1 then
+        return base_sseArray
+    else
+        array = []
+        while base_sseArray.length != 0 do
+            e = base_sseArray.shift
+            if e.class == Symbol then
+                if e == operator then
+                    array.push(operator, add_sseArray)
+                    base_sseArray.pop #末尾の削除
+                else
+                    array.push(e)
+                end
+            elsif e.class == Array then
+                array.push(add_operator_next_array(e, operator, add_sseArray))
+            else
+                array.push(e)
+            end
+        end
+        return array
+    end
+end
 
 def get_out_sseArray_in_result_bind(in_result, out_sseArray) 
 
@@ -138,7 +163,20 @@ def get_out_sseArray_in_result_bind(in_result, out_sseArray)
             bind_sseArray[1].push(add_variable)
         end
     }
-    pp bind_sseArray
+
+    #外側sseArrayにbind_sseArrayをバインドする
+    #bind_sseArrayの内容を:joinがあれば:joinが最初の要素になっている配列内の2番目の要素に格納
+    #:joinがない場合は[:bgp,~]と同じ階層に追加し、[:join, bind_sseArray, [:bgp,~]]のように変更
+
+    #1. :joinを含んでいるかをチェック
+    join_flag = out_sseArray.flatten.include?(:join)
+
+    #2.　含んでいるか含んでいないかで処理を変える
+    if join_flag then #joinがある場合
+        return add_operator_next_array(out_sseArray, :join, bind_sseArray)
+    else
+        #TO DO joinがない場合の処理を追加
+    end
 end
 
 #サブクエリのsseArrayを入力して、内側クエリを問い合わせ→結果を外側のクエリの変数にバインドし繰り返し問い合わせ→結果をマージして出力　するメソッド
@@ -169,17 +207,23 @@ def subquery_hybrid(sparql, base_sseArray)
     #サブクエリの外側sseArrayを取得するメソッド
     out_sseArray = get_out_sseArray(base_sseArray,  project_count=0) #外側sseArrayを取得(ベースのクエリ内で利用される変数オブジェクトを同じIDで取得する必要があるため、deepdupは使わない)
     
+    hybrid_subquery_result = RDF::Query::Solutions.new() #サブクエリをハイブリッド化して問い合わせた結果を格納する
+
     #内側SPARQLクエリの結果を外側SPARQLに当てはめて問い合わせ
     in_result.each_with_index{|r_in, index|
-        if index == 0 then
-            get_out_sseArray_in_result_bind(r_in, out_sseArray)
-        end
+        #内側SPARQLクエリの結果を外側sseArrayにバインド
+        out_sseArray_bind = get_out_sseArray_in_result_bind(r_in, out_sseArray.deep_dup)
+        # pp out_sseArray
+        # 内側SPARQLクエリの結果を外側sseArrayにバインドしたsseArrayをSPARQLに変換し、問い合わせ
+        out_sseArray_bind_Parsed = SPARQL::Algebra::Expression.new(out_sseArray_bind) 
+        
+        out_result = ignore_limits(sparql, out_sseArray_bind_Parsed.to_sparql)
+        
+        out_result.each{|r_out|
+            hybrid_subquery_result.push(r_out)
+        }
     }
-
-    
-    # pp out_sseArray
-    # out_sseArray_Parsed = SPARQL::Algebra::Expression.new(out_sseArray)
-    # out_result = sparql.query(out_sseArray_Parsed.to_sparql)
+    return hybrid_subquery_result
 end
 
 #エンドポイントの返り値の限界を取得するメソッド
