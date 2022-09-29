5975a006e5292315a3eae3493a28f0878bcad9fc
ichichi-3180 <watanabe.soichiro.qa@alumni.tsukuba.ac.jp>
2022-09-29 11:27:04 +0900

    apache benchを利用してベンチマーククエリの問い合わせ時間を取得

--- apache_bench/ab_SPARQLendpoint_capacity_survey.rb
+++ apache_bench/ab_SPARQLendpoint_capacity_survey.rb
@@ -42,5 +42,83 @@ endpoint_list.each {|url|
     input_file = File.open(BENCHMARK_QUERY_PATH + "count.rq", "r")
     count_query = input_file.read
     count_ms = apache_bench_test(count_query, url)
-    p count_ms
+    p "count:" + count_ms.to_s + "[ms]"
+
+    #distinctのベンチマーククエリに関してapache benchを利用して取得までの時間を取得
+    input_file = File.open(BENCHMARK_QUERY_PATH + "distinct.rq", "r")
+    distinct_query = input_file.read
+    distinct_ms = apache_bench_test(distinct_query, url)
+    p "distinct:" + distinct_ms.to_s + "[ms]"
+
+    #filterのベンチマーククエリに関してapache benchを利用して取得までの時間を取得
+    input_file = File.open(BENCHMARK_QUERY_PATH + "filter.rq", "r")
+    filter_query = input_file.read
+    filter_ms = apache_bench_test(filter_query, url)
+    p "filter:" + filter_ms.to_s + "[ms]"
+
+    #group_by_havingのベンチマーククエリに関してapache benchを利用して取得までの時間を取得
+    input_file = File.open(BENCHMARK_QUERY_PATH + "group_by_having.rq", "r")
+    group_by_having_query = input_file.read
+    group_by_having_ms = apache_bench_test(group_by_having_query, url)
+    p "group_by_having:" + group_by_having_ms.to_s + "[ms]"
+
+    #unionのベンチマーククエリに関してapache benchを利用して取得までの時間を取得
+    # TO DO ベンチマーククエリのファイルからバインドする変数情報も読み込めるようにしたい
+    # input_file = File.open(BENCHMARK_QUERY_PATH + "union.txt", "r")
+    # union_query = input_file.read
+
+    # TO DO 動的に変数名に結果の変数を割り当てる
+    domain = domain_property_result[0][:domain]
+    property = domain_property_result[0][:property]
+    union_query = """
+    SELECT ?o 
+    WHERE{
+        {?s a <#{domain}>.}
+        UNION
+        {?s <#{property}> ?o}
+    }
+    """
+    union_ms = apache_bench_test(union_query, url)
+    p "union:" + union_ms.to_s + "[ms]"
+
+    #optionalのベンチマーククエリに関してapache benchを利用して取得までの時間を取得
+    # TO DO ベンチマーククエリのファイルからバインドする変数情報も読み込めるようにしたい
+    # input_file = File.open(BENCHMARK_QUERY_PATH + "optional.txt", "r")
+    # optional_query = input_file.read
+    # TO DO 動的に変数名に結果の変数を割り当てる
+    domain = domain_property_result[0][:domain]
+    property = domain_property_result[0][:property]
+    optional_query = """
+    SELECT ?o 
+    WHERE{
+        {?s a <#{domain}>.}
+        OPTIONAL
+        {?s <#{property}> ?o}
+    }
+    """
+    optional_ms = apache_bench_test(optional_query, url)
+    p "optional:" + optional_ms.to_s + "[ms]"
+
+    #サブクエリのベンチマーククエリに関してapache benchを利用して取得までの時間を取得
+    # TO DO ベンチマーククエリのファイルからバインドする変数情報も読み込めるようにしたい
+    # input_file = File.open(BENCHMARK_QUERY_PATH + "subquery.txt", "r")
+    # subquery_query = input_file.read
+    # TO DO 動的に変数名に結果の変数を割り当てる
+    domain = class_relation_result[0][:domain]
+    range = class_relation_result[0][:range]
+    subquery = """
+    SELECT distinct ?p
+    WHERE {
+    ?s ?p ?o .
+    ?o a <#{range}> .
+        {
+            SELECT distinct ?s
+            WHERE {
+            ?s a <#{domain}>.
+            }
+        }
+    }
+    """
+    subquery_ms = apache_bench_test(subquery, url)
+    p "subquery:" + subquery_ms.to_s + "[ms]"
 }
