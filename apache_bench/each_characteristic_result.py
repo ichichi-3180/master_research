a12e2fed02580ab0b0cb3b9cf9f424bd93c8153f
ichichi-3180 <watanabe.soichiro.qa@alumni.tsukuba.ac.jp>
2022-10-12 13:29:44 +0900

    出力を棒グラフに変更

--- apache_bench/each_characteristic_result.py
+++ apache_bench/each_characteristic_result.py
@@ -1,15 +1,40 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 #データをインポート
-file_path = "./output/return_time_from_SPARQLendpoint/20221005154419.csv"
-df = pd.read_csv(file_path)
-# print(df.columns.values)
+file_path = "./output/return_time_from_SPARQLendpoint/"
+input_file_name = "20221008124229_mean10times_toPublic"
+df = pd.read_csv(file_path + input_file_name + ".csv")
+#トリプル数順でソート
+df = df.sort_values('triple_num')
+#ソート後にインデックスを降り直す
+df = df.reset_index()
 
-for e in df.columns.values[2:]: #トリプル数以降でfor文を回す
-    df[["endpoint_url",e]].plot()
-    plt.xticks(np.array(df.index), np.array(df["triple_num"])) 
-    plt.xlabel("triple num")
-    plt.ylabel("return time(s)")
-    plt.savefig(f"./output/each_characteristic_result/{e}.png")
+triple_num_label = df["triple_num"]
+endpoint_label = df["endpoint_url"].str.extract('https?://([^/]+)/',expand=False)
+
+#x軸のラベルの作成(SPARQLエンドポイントのURLのドメイン名とトリプル数)
+xlabel = np.core.defchararray.add(
+    np.core.defchararray.add(
+        np.array(endpoint_label,dtype=str),
+        np.full(len(df.index), '\n', dtype=str)
+    ),
+    np.array(triple_num_label,dtype=str)
+)
+
+
+for e in df.columns.values[1:]: #トリプル数以降でfor文を回す
+    ms = df[e]
+    print(ms)
+    plt.bar(np.array(df.index), np.array(ms), tick_label=xlabel, align="center")
+    plt.tick_params(labelsize=5)
+    plt.title(e)
+    plt.tight_layout()
+    if not os.path.exists("./output/each_characteristic_result/" + input_file_name):
+        os.makedirs("./output/each_characteristic_result/" + input_file_name)
+    plt.savefig("./output/each_characteristic_result/" + input_file_name + "/" + e + ".png")
+    #pltのメモリを解放するために以下を記述(無いとfor文の場合はpltが上書きされ続けた値が保存される)
+    plt.clf()
+    plt.close()
