e4d92289a616c531fd7b26fd33e320a0ceb7e7d9
ichichi-3180 <watanabe.soichiro.qa@alumni.tsukuba.ac.jp>
2022-12-01 09:37:44 +0900

    <add> sseの保存

--- hybrid_sample/sparql_to_sseArray.rb
+++ hybrid_sample/sparql_to_sseArray.rb
@@ -1,8 +1,8 @@
 #読み込むファイルのパスを指定
 # file_path = "../SPARQL50本ノック/SPARQL_013.sparql"
-# input_file_path = "../SPARQL_query_sample/ndl_subquery.sparql"
+input_file_path = "../SPARQL_query_sample/ndl_subquery.sparql"
 # input_file_path = "../SPARQL50本ノック/SPARQL_034.sparql"
-input_file_path = "../../SP2Bench/sp2b/queries/q3a.sparql"
+# input_file_path = "../../SP2Bench/sp2b/queries/q3a.sparql"
 input_file = File.open(input_file_path, "r")
 query = input_file.read
 
@@ -16,17 +16,23 @@ query = query.parse() #SPARQLクエリを解析
 # p query.class.ancestors
 # p query.class.method(:to_sxp_bin).owner
 # p query.class.method(:to_sxp_bin).source_location
+query_sse = query.to_sxp #SPARQL S-Expressions(SSE)
 query_sseArray = query.to_sxp_bin #解析したクエリをSPARQLクエリをsse形式のarrayに変換
 
 p query_sseArray.class
 pp query_sseArray
 #書き出すファイルの指定
-output_file_name = "SP2Bench_q3a"
+output_file_name = "ndl_subquery"
 output_path = "query_output/"
 
 #テキストとしてsseArrayを出力
 File.open(output_path + 'txt/' +  output_file_name + '.txt', 'w'){|f|
     f.write(query_sseArray)
+}
+
+#SSEを出力
+File.open(output_path + 'sse/' +  output_file_name + '.txt', 'w'){|f|
+    f.write(query_sse)
 }
 
 #jsonとしてsseArrayを出力(vscodeの拡張機能で自動整形が可能なため可読性が上がるが、クラスなどが出力されない)
